2714845ce414f48314afbace04daf6c1dc0c84b5
dalejbarr <dalejbarr3@gmail.com>
2016-01-25 12:03:30 +0000

    updated internal links in index files

--- org/publish.el
+++ org/publish.el
@@ -2,18 +2,18 @@
 (setq org-publish-project-alist
 			'(
 				("org-notes"
-				 :base-directory "~/dhome/R_TRAINING/org"
+				 :base-directory "~/dhome/R_training/org"
 				 :base-extension "org"
-				 :publishing-directory "~/talklab_html/r_training"
+				 :publishing-directory "~/dhome/R_training/html"
 				 :recursive t
 				 :publishing-function org-html-publish-to-html
 				 :headline-levels 4
 				 :auto-preamble t
 				 )
 				("org-static"
-				 :base-directory "~/dhome/R_TRAINING/org"
+				 :base-directory "~/dhome/R_training/org"
 				 :base-extension "css\\|png\\|jpg\\|gif\\|pdf\\|csv\\|rds\\|zip\\|R\\|Rmd"
-				 :publishing-directory "~/talklab_html/r_training"
+				 :publishing-directory "~/dhome/R_training/html"
 				 :recursive t
 				 :publishing-function org-publish-attachment
 				 )
